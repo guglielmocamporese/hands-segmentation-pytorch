0c1800397f68397d31bb68f413a7d14577e3382c
guglielmocamporese <guglielmocamporese@gmail.com>
2021-04-27 20:32:25 +0200

    enabled torch hub

---
+++ hubconf.py
@@ -0,0 +1,27 @@
+"""
+Torch Hub script for accessing te hand segmentation model outside the repo.
+"""
+
+##################################################
+# Imports
+##################################################
+
+from model import HandSegModel
+import os
+
+dependencies = ['torch', 'pytorch_lightning']
+
+
+def hand_segmentor(pretrained=True, *args, **kwargs):
+    """
+    Hand segmentor based on a DeepLabV3 model with a ResNet50 encoder.
+    DeeplabV3: https://arxiv.org/abs/1706.05587
+    ResNet50: https://arxiv.org/abs/1512.03385
+    """
+    model = HandSegModel(*args, **kwargs)
+    if pretrained:
+        os.system('chmod +x ./scripts/download_model_checkpoint.sh')
+        os.system('./scripts/download_model_checkpoint.sh')
+        model = model.load_from_checkpoint('./checkpoint/checkpoint.ckpt', *args, **kwargs)
+    return model
+
