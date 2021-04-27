2d5a6c44eb656ecc92e1303e79ca49b68218fbd2
guglielmocamporese <guglielmocamporese@gmail.com>
2021-04-27 20:41:13 +0200

    enabled torch hub

--- hubconf.py
+++ hubconf.py
@@ -21,8 +21,26 @@ def hand_segmentor(pretrained=True, *args, **kwargs):
     """
     model = HandSegModel(*args, **kwargs)
     if pretrained:
-        os.system('chmod +x ./scripts/download_model_checkpoint.sh')
-        os.system('./scripts/download_model_checkpoint.sh')
+        #os.system('chmod +x ./scripts/download_model_checkpoint.sh')
+        #os.system('./scripts/download_model_checkpoint.sh')
+        _download_file_from_google_drive('1w7dztGAsPHD_fl_Kv_a8qHL4eW92rlQg', 'checkpoint')
         model = model.load_from_checkpoint('./checkpoint/checkpoint.ckpt', *args, **kwargs)
     return model
+
+
+import requests
+
+def _download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)
 
